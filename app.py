323963a4a03e66769e9250ee72cee96020185c0c
koenol <olli.s.koskinen@helsinki.fi>
2026-01-21 08:35:49 +0200

    added default forms to index.html & register.html

--- app.py
+++ app.py
@@ -7,26 +7,37 @@ import service
 app = Flask(__name__)
 app.secret_key = config.SECRET_KEY
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     """Render the home page."""
-    return render_template("index.html")
+    if request.method == "GET":
+        configuration = [config.USERNAME_MIN_LENGTH, config.USERNAME_MAX_LENGTH, config.USERNAME_MIN_LENGTH, config.PASSWORD_MAX_LENGTH]
+        return render_template("index.html", config=configuration)
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
     """Register new account"""
     
-    username = request.form["username"]
-    password1 = request.form["password1"]
-    password2 = request.form["password2"]
+    if request.method == "GET":
+        configuration = [config.USERNAME_MIN_LENGTH, config.USERNAME_MAX_LENGTH, config.USERNAME_MIN_LENGTH, config.PASSWORD_MAX_LENGTH]
+        return render_template("register.html", filled={}, config=configuration)
 
-    if not service.valid_username(username):
-        flash("Username must be between 3 and 12 characters and it must contain only letters.")
-        filled = {"username": username}
-        return render_template("register.html", filled=filled)
-    
-    flash("Registration Successful. You may now log in.")
-    return redirect("/")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+
+        if not service.valid_username(username):
+            flash("Username must be between 3 and 12 characters and it must contain only letters.")
+            filled = {"username": username}
+            return render_template("register.html", filled=filled)
+        
+        flash("Registration Successful. You may now log in.")
+        return redirect("/")
+
+@app.route("/login")
+def login():
+    """Login to website"""
 
 @app.route("/ping")
 def ping():
