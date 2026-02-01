9cfd2d8356a5cf63aac202f2f8adcff4ea0bc7c6
koenol <olli.s.koskinen@helsinki.fi>
2026-02-01 16:41:19 +0200

    removed comments from database schema and renamed button to public/private for now

--- schema.sql
+++ schema.sql
@@ -28,12 +28,3 @@ CREATE TABLE statistics (
     entries_created INTEGER DEFAULT 0,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
-
-CREATE TABLE comments (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    sender_id INTEGER NOT NULL,
-    receiver_id INTEGER NOT NULL,
-    comment TEXT,
-    FOREIGN KEY (sender_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (receiver_id) REFERENCES users(id) ON DELETE CASCADE
-);
